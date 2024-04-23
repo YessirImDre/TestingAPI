ed3fd9b41f2efb0b5090987ce24427ac1b36ddae
YessirImDre <dreharrisgaming@gmail.com>
2024-04-23 19:03:14 -0400

    First version of program

--- example.js
+++ example.js
@@ -1,24 +1,21 @@
-console.trace
+// Lines of code required for console input and output
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-fetch("https://restcountries.com/v3.1/alpha/us")
+// Ask user to input the name of a country, then request the relevant data from the API
+rl.question("Enter the valid name of a country: ", (countryName) => {
+  var url = "https://restcountries.com/v3.1/name/" + countryName;
+  fetchCountry(url);
+  rl.close();
+});
+
+function fetchCountry(url)
+{
+  fetch(url)
   .then(res => res.json())
   .then(data => console.log(data))
   .catch(err => console.log("Error:", err));
-
-function initialize({
-  name,
-  capital,
-  callingCodes,
-  population,
-  currencies,
-  region
-}) {
-  console.log({
-    name,
-    capital,
-    callingCodes,
-    population,
-    currencies,
-    region
- })
 }
