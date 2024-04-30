a610479d92e27bd198cf5ef2155f0e42f63737bf
YessirImDre <dreharrisgaming@gmail.com>
2024-04-30 03:28:11 -0400

    more javascript (hope this works)

--- main.js
+++ main.js
@@ -5,22 +5,24 @@ backend.run();
 var con = backend.manageConnection;
 var userIO = backend.inputOutput;
 
-// Ask user to input the name of a country, then fetch the requested data from the database
-userIO.question("Enter the name of a country: ", (cName) => {
+function userRequest(){
+  // Ask user to input the name of a country, then fetch the requested data from the database
+  userIO.question("Enter the name of a country: ", (cName) => {
 
-  // Simple SQL Injection check
-  if (cName.toLowerCase().includes("union")){
-    console.log("Injection failed.");
-    userIO.close();
-  }
-  else{
-    con.query("SELECT * FROM countryinfo WHERE name='" + cName + "'", function (err, result, fields) {
-      if (err) throw err;
-      console.log("Name of country: " + result[0].name + '\n' +
-        "Capital: " + result[0].capital + '\n' +
-        "Continent: " + result[0].continent + '\n' +
-        "Population: " + result[0].population + '\n');
-    });
-    userIO.close();
-  }
-});
+    // Simple SQL Injection check
+    if (cName.toLowerCase().includes("union")){
+      console.log("Injection failed.");
+      userIO.close();
+    }
+    else{
+      con.query("SELECT * FROM countryinfo WHERE name='" + cName + "'", function (err, result, fields) {
+        if (err) throw err;
+        console.log("Name of country: " + result[0].name + '\n' +
+          "Capital: " + result[0].capital + '\n' +
+          "Continent: " + result[0].continent + '\n' +
+          "Population: " + result[0].population + '\n');
+      });
+      userIO.close();
+    }
+  });
+}
