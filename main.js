059636e1eb4cd17b569d3c027ccba651eeccdaff
YessirImDre <dreharrisgaming@gmail.com>
2024-04-30 04:29:05 -0400

    lets hope this works

--- main.js
+++ main.js
@@ -6,41 +6,24 @@ var con = backend.manageConnection;
 var userIO = backend.inputOutput;
 
 function userRequest(){
-  // Ask user to input the name of a country, then fetch the requested data from the database
-  userIO.question("Enter the name of a country: ", (cName) => {
 
-    // Simple SQL Injection check
-    if (cName.toLowerCase().includes("union")){
-      console.log("Injection failed.");
-      userIO.close();
-    }
-    else{
-      con.query("SELECT * FROM countryinfo WHERE name='" + cName + "'", function (err, result, fields) {
-        if (err) throw err;
+  var name = document.getElementById("request").value;
 
-        <table>
-          <tr>
-            <th>Name</th>
-            <th>Capital</th>
-            <th>Region</th>
-            <th>Population</th>
-          </tr>
-          <tr>
-            <th>result[0].name</th>
-            <th>result[0].capital</th>
-            <th>result[0].continent</th>
-            <th>result[0].population</th>
-          </tr>
-        </table>
-
-        console.log("Name of country: " + result[0].name + '\n' +
-          "Capital: " + result[0].capital + '\n' +
-          "Continent: " + result[0].continent + '\n' +
-          "Population: " + result[0].population + '\n');
-      });
-      userIO.close();
-    }
-  });
-
-
-}
+  if (name.toLowerCase().includes("union")){
+    console.log("Injection failed.");
+    userIO.close();
+  }
+  else{
+    con.query("SELECT * FROM countryinfo WHERE name='" + name + "'", function (err, result, fields) {
+      if (err) throw err;
+  
+      var rows = "<tr><td>" + result[0].name + "</td><td>" + result[0].capital + "</td><td>" + result[0].continent + "</td><td>" + result[0].population + "</td></tr>";
+      $(rows).appendTo("#country tbody");
+  
+      console.log("Name of country: " + result[0].name + '\n' +
+        "Capital: " + result[0].capital + '\n' +
+        "Continent: " + result[0].continent + '\n' +
+        "Population: " + result[0].population + '\n');
+    });
+  }
+};
